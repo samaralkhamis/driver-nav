8f775b2365265a0630b6e50f376c63fcbef2eaca
Taebah shlouh <119taeba@gmail.com>
2022-04-06 11:48:57 +0300

    array reverse

---
+++ array/app.js
@@ -0,0 +1,11 @@
+
+
+function arr_re(array) {
+    let arr=[];
+    for (let i = array.length;i>0; i--) {
+       arr.push(array.pop());
+     
+    }
+    return arr;
+}
+console.log(arr_re([2,4,6,1,6]));
