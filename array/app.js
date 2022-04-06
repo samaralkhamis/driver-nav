8d988d8506ea1f23e66e1041271e54fa5a5236c9
Taebah shlouh <119taeba@gmail.com>
2022-04-06 19:48:02 +0300

    driver-nav tasks

--- array/app.js
+++ array/app.js
@@ -1,27 +1,20 @@
-
+"use strict";
+//////////////////////////////////////////////////////
 
 function arr_re(array) {
     let arr=[];
     for (let i = array.length;i>0; i--) {
        arr.push(array.pop());
-     
     }
     return arr;
 }
+let t1=arr_re([2,4,6,1,6]);
 console.log(arr_re([2,4,6,1,6]));
 
-// function upperCase(x) {
-//   var array=x.split('');
-//   var array1=[];
-//   for(var i=0;i<array.lenght;i++)
-//   {
-//       array[i].charAt(0).toUpperCase()+array1[i].slice(1);
-//   }
-//   return array1;
-// }
-// console.log(upperCase("the car drove fast"));
 
-
+document.getElementById("round1").textContent="First Round: Taebah was the driver and Samar was the navigator";
+document.getElementById("task1").innerHTML= `${t1}`;
+///////////////////////////////////////////////////////
 
 function uppercaseWords(string){
     var words  = string.split(" ");
@@ -33,5 +26,45 @@ function uppercaseWords(string){
     }
     console.log(words.join(" "));
 }
-uppercaseWords('the quick brown fox');
-uppercaseWords("the car drove fast");
+ uppercaseWords("the car drove fast");
+
+ document.getElementById("round2").textContent="Second Round: Taebah was the driver and Samar was the navigator";
+ document.getElementById("task2").innerHTML="The Car Drove Fast";
+////////////////////////////////////////////////////////////
+let st="ABCDCBA";
+
+function isPalindrome(str) {
+    for(var i = 0, j = str.length - 1; i < str.length; i++, j--) {
+      if (str[i] !== str[j]) return false
+    }
+  
+    return true
+  }
+ console.log(isPalindrome(st)); 
+
+ document.getElementById("round3").textContent="Third Round: Taebah was the navigator and Samar was the driver";
+ document.getElementById("task3").innerHTML=JSON.stringify(isPalindrome(st));
+///////////////////////////////////////////////////////////
+
+  let x=[2,9,0,4];
+
+  function arraySort(array) {
+    var done = false;
+    while (!done) {
+      done = true;
+      for (var i = 1; i < array.length; i += 1) {
+        if (array[i - 1] > array[i]) {
+          done = false;
+          var sw = array[i - 1];
+          array[i - 1] = array[i];
+          array[i] = sw;
+        }
+      }
+    }
+  
+    return array;
+  }
+  console.log(arraySort(x));
+  /////////////////////////////////////////////////////////
+ document.getElementById("round4").textContent="Fourth Round: Taebah was the navigator and Samar was the driver";
+ document.getElementById("task4").innerHTML=JSON.stringify(arraySort(x));
