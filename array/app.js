fe1afb3023615c81ed89c1586157a76f08ad527c
Taebah shlouh <119taeba@gmail.com>
2022-04-06 19:58:06 +0300

    tasks

--- array/app.js
+++ array/app.js
@@ -11,15 +11,11 @@ function arr_re(array) {
 let t1=arr_re([2,4,6,1,6]);
 console.log(arr_re([2,4,6,1,6]));
 
-<<<<<<< HEAD
 
 document.getElementById("round1").textContent="First Round: Taebah was the driver and Samar was the navigator";
 document.getElementById("task1").innerHTML= `${t1}`;
 ///////////////////////////////////////////////////////
 
-=======
-///////////////////////////////////////////////////////
->>>>>>> 862a96e6c4deceb00101b8dfbe85a88cb23a28e9
 function uppercaseWords(string){
     var words  = string.split(" ");
     for (var i=0; i < words.length; i++){
@@ -30,7 +26,6 @@ function uppercaseWords(string){
     }
     console.log(words.join(" "));
 }
-<<<<<<< HEAD
  uppercaseWords("the car drove fast");
 
  document.getElementById("round2").textContent="Second Round: Taebah was the driver and Samar was the navigator";
@@ -73,20 +68,3 @@ function isPalindrome(str) {
   /////////////////////////////////////////////////////////
  document.getElementById("round4").textContent="Fourth Round: Taebah was the navigator and Samar was the driver";
  document.getElementById("task4").innerHTML=JSON.stringify(arraySort(x));
-=======
-uppercaseWords('the quick brown fox');
-uppercaseWords("the car drove fast");
-///////////////////////////////////////////////////////////
-
- let str="ABCDCBA"
- console.log(str);
- palindrome(str);
-
-function palindrome (word){
-let leng= Math.floor((word.length)/2);
-for (let i= 0; i < leng; i++) {
-    if (word[i] !== word.lengnth-1){
-        console.log(`the ${word[i]} == ${word.lengnth-1} `);
-    }}
-console.log ("the word is palindrome" );}
->>>>>>> 862a96e6c4deceb00101b8dfbe85a88cb23a28e9
